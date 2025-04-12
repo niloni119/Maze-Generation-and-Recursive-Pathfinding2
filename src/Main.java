36548270fd652f65fcc789390c848a4811513d86
User <git@github.com:niloni119/newrepo.git>
2025-04-12 17:31:27 +0600

    added constructor to set up the maze size and call generator

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
 import java.util.*;
 
 public class Main {
-    // variables of maze(wall, path, size of maze, array, random digit, entrance, exit)
     private static final int W = 1;
     private static final int P = 0;
     private int n;
     private int[][] m;
     private Random r = new Random();
-    private int sx = 0, sy = 1; // entrance's coordinates
-    private int ex, ey; // exit's coordinates
+    private int sx = 0, sy = 1;
+    private int ex, ey;
+
+    public Main(int n) {
+        this.n = n + 2;
+        this.m = new int[this.n][this.n];
+        this.ex = this.n - 1;
+        this.ey = this.n - 2;
+        gen(); // generate maze right after creation
+    }
+
+    private void gen() {
+        // will implement later
+    }
 }
