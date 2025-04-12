81c6187657ce125c604d6f3ea65ae49f83944553
User <git@github.com:niloni119/newrepo.git>
2025-04-12 17:32:28 +0600

    wrote the maze generation loop

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,33 @@ public class Main {
         Stack<int[]> s = new Stack<>();
         m[x][y] = P;
         s.push(new int[]{x, y});
+
+        int[][] d = {{0, 2}, {2, 0}, {-2, 0}, {0, -2}};
+
+        while (!s.isEmpty()) {
+            int[] c = s.peek();
+            x = c[0];
+            y = c[1];
+            ArrayList<int[]> ns = new ArrayList<>();
+
+            for (int[] i : d) {
+                int nx = x + i[0];
+                int ny = y + i[1];
+                if (nx > 0 && ny > 0 && nx < n - 1 && ny < n - 1 && m[ny][nx] == W) {
+                    ns.add(new int[]{nx, ny});
+                }
+            }
+
+            if (!ns.isEmpty()) {
+                int[] nt = ns.get(r.nextInt(ns.size()));
+                int mx = (x + nt[0]) / 2;
+                int my = (y + nt[1]) / 2;
+                m[my][mx] = P;
+                m[nt[1]][nt[0]] = P;
+                s.push(nt);
+            } else {
+                s.pop();
+            }
+        }
     }
 }
