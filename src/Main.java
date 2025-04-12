a83a97718b562471fd1dac4f7f7522280d72d6e1
User <git@github.com:niloni119/newrepo.git>
2025-04-12 17:43:32 +0600

    fixed the maze generation and updated the path printing logic

--- src/Main.java
+++ src/Main.java
@@ -130,6 +130,6 @@ public class Main {
         int n = 11;
         Main mg = new Main(n);
         ArrayList<int[]> p = mg.sol();
-        mg.prt(p);
+        mg.prt(p);  // print the maze with the solution path
     }
 }
