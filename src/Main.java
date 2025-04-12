b09f7d510f8e1f478e74db8706544c7f1280ec05
User <git@github.com:niloni119/newrepo.git>
2025-04-12 17:30:42 +0600

    Created basic maze class and added some fields

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    // variables of maze(wall, path, size of maze, array, random digit, entrance, exit)
+    private static final int W = 1;
+    private static final int P = 0;
+    private int n;
+    private int[][] m;
+    private Random r = new Random();
+    private int sx = 0, sy = 1; // entrance's coordinates
+    private int ex, ey; // exit's coordinates
+}
